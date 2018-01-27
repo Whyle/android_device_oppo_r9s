9528cda94a74b6a098237d9bfdbafc40707141a6
Yihao <yihao.zhuo.2001@gmail.com>
2018-01-27 14:06:45 +0100

    R9s: try to fix wlan

--- device.mk
+++ device.mk
@@ -311,7 +311,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-
+   $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/wifi/WCNSS_wlan_dictionary.dat \
+ 
 # Keyhandler
 PRODUCT_PACKAGES += \
     ConfigPanel \
